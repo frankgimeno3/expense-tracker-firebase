1f80517ecebfb4fdfd79d82f7d2da0c0201b28c0
frank gimeno <frankgimeno3@gmail.com>
2023-10-24 12:15:02 +0200

    soluciono problema firebase

--- app/firebase.js
+++ app/firebase.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import firebase from "firebase/compat/app";
+import { getFirestore } from 'firebase/firestore';
 
-import "firebase/firestore";
-import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDvpmo-9U8-2jDFBm9rTkjGHeMAvMi17NU",
@@ -16,6 +14,7 @@ const firebaseConfig = {
   appId: "1:1018467887876:web:a2bbc2ac14a2d440e2fe51"
 };
 
+  
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+export const db = getFirestore(app);
